0b2197c573fc4d3d018a4ff3fddb497098044b37
tastybento <tastybento@wasteofplastic.com>
2021-08-21 15:03:24 -0700

    Initial world

---
+++ src/main/java/world/bentobox/parkour/generators/ChunkGeneratorWorld.java
@@ -0,0 +1,40 @@
+package world.bentobox.parkour.generators;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+import org.bukkit.World;
+import org.bukkit.generator.BlockPopulator;
+import org.bukkit.generator.ChunkGenerator;
+
+import world.bentobox.parkour.Parkour;
+
+/**
+ * @author tastybento
+ *         Creates the world
+ */
+public class ChunkGeneratorWorld extends ChunkGenerator {
+
+    private final Parkour addon;
+
+    /**
+     * @param addon - addon
+     */
+    public ChunkGeneratorWorld(Parkour addon) {
+        super();
+        this.addon = addon;
+    }
+
+
+    @Override
+    public ChunkData generateChunkData(World world, Random random, int chunkX, int chunkZ, BiomeGrid biomeGrid) {
+        return createChunkData(world);
+    }
+
+    @Override
+    public List<BlockPopulator> getDefaultPopulators(final World world) {
+        return Collections.emptyList();
+    }
+
+}
